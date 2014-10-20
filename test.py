577bb64d9ab9d99745afe6989bd851e2c46f0240
lxserenade <lixia625@hotmail.com>
2014-10-20 15:33:58 +0800

    get comment count

--- test.py
+++ test.py
@@ -2,50 +2,52 @@
 #-*- coding:utf8 -*-
 import os,sys
 import urllib
-import urllib2
 import re
-from HTMLParser import HTMLParser
+import json
+import csv
 
-from bs4 import BeautifulSoup
-# path=os.getcwd()+"\\"
-# filename="test.shtml"
-# f=open(path+filename,"r")
-# soup = BeautifulSoup(f)
-
-# url='http://news.sina.com.cn/c/2014-10-18/014031007028.shtml'
-
-# data=urllib.urlopen(url).read().decode('gb2312','ignore')
-
-# soup=BeautifulSoup(data)
-# print data
-
-
-
-
-def processurl(url):
-	global num
-	num=num+1
+########################
+#return {{u'qreply': int, u'total': int, u'show': int}}
+# total:参与人数（评论＋点赞or支持＋回复数），show：评论数 qreply：回复数
+def get_news_comment(url):
+    #html data
 	data=urllib.urlopen(url).read().decode('gb2312','ignore')
-	#print data
 
-#=================================================
+	try:
+		#get channel,newsid
+		channel_pattern = re.compile(r'channel:\'(.*?)\',')
+		channel = channel_pattern.search(data).group(1)
+		newsid_pattern = re.compile(r'newsid:\'(.*?)\',')
+		newsid = newsid_pattern.search(data).group(1)
+	except:
+		return '{can not find channel or newsid}'
+	#get comment count from json_data
+	comment_url='http://comment5.news.sina.com.cn/page/info?format=js&channel='+channel+'&newsid='+newsid
+	comment_data=urllib.urlopen(comment_url).read().decode('gb2312','ignore')
+	comment=json.loads(comment_data[9:])
+	return comment["result"]["count"]
+#########################
+
+
+#########################
 # 递归遍历指定的目录  
 # path-遍历起始绝对路径  
-
-
 TargetFileType=['html','shtml']
+log=[]
 num=0
-def listyoudir(path):  
+def process(path):  
 	global num
 	for i in os.listdir(path):
 		if not os.path.isdir(path + '\\' + i):
 			if i.split('.')[-1].lower() in TargetFileType:
 				url='http:/'+(path + '\\' + i)[8:].replace('\\','/')
-				processurl(url)
+				log.addpend(get_news_comment(url))
 		else:
 			listyoudir(path + '\\'+i)
 
+
+
 #测试代码  
 rootpath = os.path.abspath('.')  
-listyoudir(rootpath)
-print num
+process(rootpath)
+print log
