efd61b7adf12551921a79508d52c4966d8a43951
lxserenade <lixia625@hotmail.com>
2014-10-21 17:14:31 +0800

    version 1.0

--- test.py
+++ test.py
@@ -5,30 +5,32 @@ import urllib
 import re
 import json
 import csv
+import socket
 
+socket.setdefaulttimeout(10) # 10 秒钟后超时
 ########################
 #return {{u'qreply': int, u'total': int, u'show': int}}
 # total:参与人数（评论＋点赞or支持＋回复数），show：评论数 qreply：回复数
 def get_news_comment(url):
-    #html data
-	data=urllib.urlopen(url).read().decode('gb2312','ignore')
-
-	try:
+    try:
+		#html data
+		data=urllib.urlopen(url).read().decode('gb2312','ignore')
 		#get channel,newsid
 		channel_pattern = re.compile(r'channel:\'(.*?)\',')
 		channel = channel_pattern.search(data).group(1)
 		newsid_pattern = re.compile(r'newsid:\'(.*?)\',')
 		newsid = newsid_pattern.search(data).group(1)
-	except:
-		return '{can not find channel or newsid}'
-	#get comment count from json_data
-	comment_url='http://comment5.news.sina.com.cn/page/info?format=js&channel='+channel+'&newsid='+newsid
-	comment_data=urllib.urlopen(comment_url).read().decode('gb2312','ignore')
-	comment=json.loads(comment_data[9:])
-	return comment["result"]["count"]
+		#get comment count from json_data
+		comment_url='http://comment5.news.sina.com.cn/page/info?format=js&channel='+channel+'&newsid='+newsid
+		comment_data=urllib.urlopen(comment_url).read().decode('gb2312','ignore')
+		comment=json.loads(comment_data[9:])
+		com=comment["result"]["count"]
+    except:
+		return "{{u'qreply': None, u'total': None, u'show': None}}"
+    return comment["result"]["count"]
 #########################
 
-
+num=0
 #########################
 # 递归遍历指定的目录  
 # path-遍历起始绝对路径  
@@ -39,13 +41,28 @@ def process(path):
 		if not os.path.isdir(path + '\\' + i):
 			if i.split('.')[-1].lower() in TargetFileType:
 				url='http:/'+(path + '\\' + i)[8:].replace('\\','/')
-				log.addpend(get_news_comment(url))
+				com=get_news_comment(url)
+				print com
+				global log
+				log.append(com)
+				global num
+				num=num+1
+				if num%20==0:
+					print '----------------------------------'
+					print num
+					print '----------------------------------'
+					
 		else:
-			listyoudir(path + '\\'+i)
+			process(path + '\\'+i)
 
 
 
 #测试代码  
-rootpath = os.path.abspath('.')  
+rootpath = os.path.abspath('D:\\LX\\li\\')  
 process(rootpath)
-print log
+print len(log)
+fl=open('log.txt','w')
+for i in log:
+    fl.write(str(i))
+    fl.write("\n")
+fl.close()
