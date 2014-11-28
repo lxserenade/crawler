b14bea8967e986e313f26d2bab589656eb98a574
lixia <lixia625@hotmail.com>
2014-11-29 01:45:19 +0800

    plot

---
+++ video_data_process.py
@@ -0,0 +1,19 @@
+
+from  matplotlib import pyplot as plt
+
+f=open('video_data.txt','r')
+
+lines=f.readlines()
+data=[]
+for line in lines:
+	li=eval(line)
+	for item in li:
+		data.append(item)
+
+print len(data)
+
+
+a1=[x[0] for x in data if  x[0]]
+a2=[int(x[1].replace(',','')) for x in data if  x[0]]
+plt.plot(a1,a2,'ro')
+plt.show()
