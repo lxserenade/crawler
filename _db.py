2fd1eadff5647f288e3415146896b93435188705
lxserenade <lixia625@hotmail.com>
2014-12-12 15:04:52 +0800

    update data

--- _db.py
+++ _db.py
@@ -35,19 +35,24 @@ def getDays(beginDate,endDate):
 #  main process
 #  import data to db
 
-f = open('video_data.txt', 'r')
+f = open('video_data_2010-2014.txt', 'r')
 
 lines = f.readlines()
 
-
+print len(lines)
+print len(getDays("20101127","20141127"))
+raw_input("asd")
 conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='sina',charset="utf8")
 cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
 sql = "insert into sina_top_video(date,comments,view) values(%s,%s,%s)"
 i=0
-for date in getDays("20131127","20141126"):
-	line=lines[i]
-	li = eval(line)
-	for item in li:
+for date in getDays("20101127","20141127"):
+    line=lines[i]
+    i = i+1
+    if i>=len(lines):
+        break
+    li = eval(line)
+    for item in li:
 		tmp=(date,item[0],item[1].replace(',', ''))
 		cursor.execute(sql,tmp)
 cursor.close()
